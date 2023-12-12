21f2ea4633d32079f975e9c5fe1d46bc0da6a328
Nargis Fatime <nargis.fatima160@gmail.com>
2023-12-12 19:28:57 +0530

    javaScript

---
+++ function.js
@@ -0,0 +1,9 @@
+function callMe(){
+    var data="miraj1"
+    let data2="nargis"
+    const data3="Fatima"
+    console.log("var example==",data);
+    console.log("let example==",data2);
+    console.log("const example==",data3)
+}
+callMe()
